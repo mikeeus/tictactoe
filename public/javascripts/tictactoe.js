686ada4353d07fc1d7960b451b954a2addc1de86
mikeeus <itsmikias@gmail.com>
2016-11-14 12:36:12 +0300

    Complete missing features

--- public/javascripts/tictactoe.js
+++ public/javascripts/tictactoe.js
@@ -1,5 +1,9 @@
 $(document).ready(function(){
   
+   $('.x').click(function(){
+     $('.x').off("click");
+   });  
+  // $('.o').hide();
 });
 
 var turn; // "Player One" || "Player Two" || "Computer"
@@ -11,6 +15,10 @@ var score = {
 }; // {one: 0, two: 0, computer: 0}
 var playing; // game state, true or false
 var moves = [];
+var computerSign = 'O';
+var playerOneSign = 'X';
+var playerTwoSign = 'O';
+var signSelected = false;
 var squares = ['sq1', 'sq2', 'sq3', 'sq4', 'sq5', 'sq6', 'sq7', 'sq8', 'sq9'];
 var rows = [
     ['sq1', 'sq2', 'sq3'],
@@ -27,13 +35,35 @@ var message = "Start game?";
 
 function startGame(){
   resetBoard();
-  mode === 'single-player' ? turn = "Computer" : turn = "Player Two";
-  playing = true;
-  console.log('playing: ', playing);
-  console.log('turn: ', turn);
-  console.log('start game!');
-  computerMove();
-  updateDisplay();
+  if(signSelected){
+    if( mode === 'single-player'){
+      turn = "Computer";
+      computerMove();
+    } else if( mode === 'two-player'){
+      turn = "Player Two";
+    }
+    playing = true;
+    updateDisplay();
+  } else {
+    $('#message').fadeOut(1000);
+    $('#message-text').hide();
+    $('#select-sign').fadeIn(500);
+  }
+}
+function selectSign(sign){
+  if(!signSelected){
+    playerOneSign = sign;
+    if(sign === 'X'){
+      playerTwoSign = 'O';
+      computerSign = 'O';
+    } else if(sign === 'O'){
+      playerTwoSign = 'X';
+      computerSign = 'X';
+    }
+    $('#select-sign').fadeOut(500);
+    signSelected = true;
+    startGame();
+  }
 }
 function resetBoard(){
   moves = [];
@@ -55,40 +85,41 @@ function resetBoard(){
       element.html('');
     }
   });
-  console.log('reset!');
 }
 function resetScore(){
   score = {one: 0, two: 0, computer: 0};
 }
 function changeMode(type){
-  console.log('mode: ', type);
   $('#mode-single').removeClass('selected');
   $('#mode-two').removeClass('selected');  
   mode = type;
   type == 'single-player' ? $('#mode-single').addClass('selected') : $('#mode-two').addClass('selected');
   resetScore();
+  $('#message-text').html('Start Game');
+  updateDisplay();
 }
 
-function toggleTouch(square, computer){
-  console.log(square);
+function playerMove(square){
   if(!playing){ console.log('Start game!'); return; };
-  if(turn == 'Computer' && !computer){ console.log("computer's turn!"); return;}
+  if(turn == 'Computer'){ console.log("computer's turn!"); return;}
   element = $('#' + square);
-  if(element.hasClass('untouched')){
+  if(element.hasClass('untouched') && playing && turn != 'Computer'){
     moves.push(square);
     updateState(square, turn);
-    turn == 'Computer' ? element.html('O'): element.html('X');
+    turn === 'Player One' ? element.html(playerOneSign) : element.html(playerTwoSign);
     element.removeClass('untouched');
     element.addClass('touched');
+    if(mode === 'single-player'){ 
+      turn = 'Computer';
+      computerMove();
+    } else if (mode === 'two-player'){
+      turn === 'Player One' ? turn = 'Player Two' : turn = 'Player One';
+    }
+    updateDisplay();
+    checkWinner();
+  } else if(element.hasClass('touched')){
+    return;
   }
-  if(mode == 'single-player'){
-    turn == 'Player One' ? turn = 'Computer' : turn = 'Player One';
-  } else if(mode == 'two-player'){
-    turn == 'Player One' ? turn = 'Player Two' : turn = 'Player One';
-  }
-  updateDisplay();
-  if(turn == 'Computer'){ computerMove(); };
-  checkWinner();
 }
 function updateState(square, turn){
   for(i = 0; i < 3; i++){
@@ -104,9 +135,10 @@ function updateState(square, turn){
 }
 
 function updateDisplay(){
-  playing ? $('#controls').hide() : $('#controls').show();
-  playing ? $('#in-game').show() : $('#in-game').hide();
-  playing ? $('#message').hide() : $('#message').show();
+  playing ? $('#controls').fadeOut(1000) : $('#controls').fadeIn(1000);
+  $('#in-game').show();
+  playing ? $('#message').fadeOut(1000) : $('#message').fadeIn(1000);
+  playing ? $('#message-text').hide() : $('#message-text').show();
   if(mode == 'single-player'){
     $('#player-two').html('Computor');
     // update score
@@ -117,13 +149,27 @@ function updateDisplay(){
     $('#player-one-score').html(score['one']);
     $('#player-two-score').html(score['two']);
   }
-  $('#turn').html(turn);
+  $('#turn-name').html(turn);
 }
 
 function computerMove(){
   function move(square){
     setTimeout(function(){
-      toggleTouch(square, true);
+      element = $('#' + square);
+      if(element.hasClass('untouched')){
+        moves.push(square);
+        updateState(square, 'Computer');
+        element.html(computerSign);
+        element.removeClass('untouched');
+        element.addClass('touched');
+      }
+      if(mode == 'single-player'){
+        turn == 'Player One' ? turn = 'Computer' : turn = 'Player One';
+      } else if(mode == 'two-player'){
+        turn == 'Player One' ? turn = 'Player Two' : turn = 'Player One';
+      }
+      updateDisplay();
+      checkWinner();
     }, 1000);
   }
   if(checkPattern() !== undefined){
@@ -134,10 +180,7 @@ function computerMove(){
     case 0:
       // Always pick the corner
       rand = Math.floor(Math.random()*3);
-      start = corner[rand];
-      console.log('rand: ', rand);
-      console.log('start: ', start);
-      move(start);
+      move(corner[rand]);
       break;
     case 2:
       if (moves[1] === 'sq5' && moves[0] === 'sq1'){ move('sq3'); }
@@ -207,14 +250,12 @@ function checkWin(winner, opponent){
   winningSquare = undefined;
   // check diagonal 1
   if(countInArray(diagonal1, winner) === 2 && countInArray(diagonal1, opponent) === 0){
-    console.log('diagonal1 opportunity: ', winner);
     diagonal1.map(function(square){
       if(square !== winner){ winningSquare = square; }
     })
   }
   // check diagonal 2
   if(countInArray(diagonal2, winner) === 2 && countInArray(diagonal2, opponent) === 0){
-    console.log('diagonal2 opportunity: ', winner);
     diagonal2.map(function(square){
       if(square !== winner){ winningSquare = square; }
     })
@@ -222,25 +263,17 @@ function checkWin(winner, opponent){
   // check rows and columns
   for(i = 0; i < 3; i++){
     if(countInArray(rows[i], winner) === 2 && countInArray(rows[i], opponent) === 0){
-      console.log('row',i, ': ', rows[i]);
       rows[i].map(function(square){
-        if(square !== winner){
-          console.log('opportunity at: ', square);
-          winningSquare = square;
-        }
+        if(square !== winner){ winningSquare = square; }
       })
     }
     if(countInArray(cols[i], winner) === 2 && countInArray(cols[i], opponent) === 0){
       cols[i].map(function(square){
-        if(square !== winner){
-          console.log('opportunity at: ', square);
-          winningSquare = square;
-        }
+        if(square !== winner){ winningSquare = square; }
       })
     }
     if(winningSquare !== undefined){ break; };
   }
-  console.log('winningSquare: ', winningSquare);
   return winningSquare;
 } 
 // Check to see if chain of 3 is possible
@@ -256,12 +289,8 @@ function checkPattern(){
     // if not, check if player can and block
   } else if(compWin === undefined){
     playerThreat = checkWin('Player One', 'Computer');
-    if(playerThreat !== undefined){
-      console.log('Player threat: ', playerThreat);
-      pattern = playerThreat;
-    }
+    if(playerThreat !== undefined){ pattern = playerThreat; }
   }
-
   return pattern;
 }
 // Get best move for computer
@@ -271,36 +300,24 @@ function getBestMove(){
   bestMove = undefined;
   if(countInArray(diagonal1, 'Computer') > 0 && countInArray(diagonal1, 'Player One') === 0){
     diagonal1.map(function(square){
-      if(square !== 'Computer'){
-        bestMove = square;
-        console.log('Best move is: ', square);
-      }
+      if(square !== 'Computer'){ bestMove = square; }
     })
   }
   if(countInArray(diagonal2, 'Computer') > 0 && countInArray(diagonal2, 'Player One') === 0){
     diagonal2.map(function(square){
-      if(square !== 'Computer'){
-        bestMove = square;
-        console.log('Best move is: ', square);
-      }
+      if(square !== 'Computer'){ bestMove = square; }
     })
   }
   for(i = 0; i < 3; i++){
     // check if player has opportunity(third)
     if(countInArray(rows[i], 'Computer') > 0 && countInArray(rows[i], 'Player One') === 0){
       rows[i].map(function(square){
-        if(square !== 'Computer'){
-          bestMove = square;
-          console.log('Best move is: ', square);
-        }
+        if(square !== 'Computer'){ bestMove = square; }
       })
     }
     if(countInArray(cols[i], 'Computer') > 0 && countInArray(cols[i], 'Player One') === 0){
       cols[i].map(function(square){
-        if(square !== 'Computer'){
-          bestMove = square;
-          console.log('Best move is: ', square);
-        }
+        if(square !== 'Computer'){ bestMove = square; }
       })
     }
   }
@@ -313,26 +330,26 @@ function checkWinner(){
   if(mode === 'single-player'){
     if(checkThreeInARow('Computer')){
       playing = false;
-      $('#message').html('Computer wins!');
+      $('#message-text').html('Computer wins! <br> Play Again?');
       score.computer += 1;
     } else if(checkThreeInARow('Player One')){
       playing = false;
-      $('#message').html('Player One wins!');
+      $('#message-text').html('Player One wins! <br> Play Again?');
       score.one += 1;
     }
   } else if(mode === 'two-player'){
     if(checkThreeInARow('Player Two')){
       playing = false;
-      $('#message').html('Player Two wins!');
+      $('#message-text').html('Player Two wins! <br> Play Again?');
       score.two += 1;      
     } else if(checkThreeInARow('Player One')){
       playing = false;
-      $('#message').html('Player One wins!');
+      $('#message-text').html('Player One wins! <br> Play Again?');
       score.one += 1;
     }
   }
   if(moves.length === 9 && playing === true){
-    console.log("It's a tie!");
+    $('#message-text').html("It's a tie! <br> Play again?")
     playing = false;
   } 
   updateDisplay();
@@ -345,59 +362,14 @@ function checkThreeInARow(player){
   diagonal1 = [rows[0][0], rows[1][1], rows[2][2]];
   diagonal2 = [rows[0][2], rows[1][1], rows[2][0]];
   if(countInArray(diagonal1, player) === 3 || countInArray(diagonal2, player) === 3){
-    console.log(player, ' wins!');
     playerWon = true;
   } else{
     for(i = 0; i < 3; i++){
       // check if player has opportunity(threat)
       if(countInArray(rows[i], player) === 3 || countInArray(cols[i], player) === 3){
-        console.log(player, ' wins!');
         playerWon = true;
       }
     }
   }
   return playerWon;
 }
-
-
-  // if(countInArray(diagonal1, 'Computer') === 3 || countInArray(diagonal2, 'Computer') === 3){
-  //   console.log('Computer wins!');
-  //   playing = false;
-  //   return;
-  // } else if(countInArray(diagonal1, 'Player One') === 3 || countInArray(diagonal2, 'Player One') === 3){
-  //   console.log('Player One wins!');
-  //   playing = false;
-  //   return; 
-  // } else if(countInArray(diagonal1, 'Player Two') === 3 || countInArray(diagonal2, 'Player Two') === 3){
-  //   console.log('Player One wins!');
-  //   playing = false;
-  //   return; 
-  // }
-  // for(i = 0; i < 3; i++){
-  //   // check if player has opportunity(threat)
-  //   if(countInArray(rows[i], 'Computer') === 3){
-  //     console.log('Computer wins!');
-  //     playing = false;
-  //     return;
-  //   } else if(countInArray(cols[i], 'Computer') === 3){
-  //     console.log('Computer wins!');
-  //     playing = false;
-  //     return;
-  //   } else if(countInArray(rows[i], 'Player One') === 3){
-  //     console.log('Player One wins!');
-  //     playing = false;
-  //     return;      
-  //   } else if(countInArray(cols[i], 'Player One') === 3){
-  //     console.log('Player One wins!');
-  //     playing = false;
-  //     return;      
-  //   } else if(countInArray(rows[i], 'Player Two') === 3){
-  //     console.log('Player One wins!');
-  //     playing = false;
-  //     return;      
-  //   } else if(countInArray(cols[i], 'Player Two') === 3){
-  //     console.log('Player Two wins!');
-  //     playing = false;
-  //     return;      
-  //   }
-  // }
